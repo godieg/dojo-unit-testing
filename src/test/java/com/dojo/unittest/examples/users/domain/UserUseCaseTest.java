b6837dc869d8d0f830fef0dd52e6812307e1d787
godieg <godie100@gmail.com>
2023-12-14 09:38:45 -0500

    Ajuste de codigo

--- src/test/java/com/dojo/unittest/examples/users/domain/UserUseCaseTest.java
+++ src/test/java/com/dojo/unittest/examples/users/domain/UserUseCaseTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-public class UserUseCaseTest {
+class UserUseCaseTest {
 
     @Mock
     UserRepository userRepository;
